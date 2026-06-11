711a59dbdd23e37ba9316810b9996f298af938f2
tipharez-allmighty <artemkolos@gmail.com>
2026-06-11 23:25:06 +0800

    search structs

--- internal/response.go
+++ internal/response.go
@@ -4,17 +4,23 @@ type YoutubeResponse interface {
 	URL() string
 }
 
+type SearchID struct {
+	Kind    string `json:"kind"`
+	VideoID string `json:"videoId"`
+}
+
+type SearchSnippet struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+}
+
+type SearchItem struct {
+	ID      SearchID      `json:"id"`
+	Snippet SearchSnippet `json:"snippet"`
+}
+
 type SearchResponse struct {
-	Items []struct {
-		ID struct {
-			Kind    string `json:"kind"`
-			VideoID string `json:"videoId"`
-		} `json:"id"`
-		Snippet struct {
-			Title       string `json:"title"`
-			Description string `json:"description"`
-		} `json:"snippet"`
-	} `json:"items"`
+	Items []SearchItem `json:"items"`
 }
 
 func (*SearchResponse) URL() string { return "search" }
